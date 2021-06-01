da010a8e739a798c17a5f33e92dbb7c49db17a45
VivienLN <vivienleneez@gmail.com>
2021-06-01 15:30:40 +0200

    fix watch/poll in docker

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 module.exports = {
-    images: {
-      domains: ['placehold.it'],
-    },
+  images: {
+    domains: ['placehold.it'],
+  },
+  webpackDevMiddleware: (config) => {
+    // Solve compiling problem via vagrant
+    config.watchOptions = {
+      poll: 1000,   // Check for changes every second
+      aggregateTimeout: 300,   // delay before rebuilding
+    };
+    return config;
   }
+}
