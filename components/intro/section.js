b50624955c7f3ab5b95d2c717d587241b01432a5
VivienLN <vivienleneez@gmail.com>
2021-06-22 17:30:07 +0200

    intro wip

--- components/intro/section.js
+++ components/intro/section.js
@@ -7,10 +7,10 @@ export default function Section() {
         <header className="relative">
             <style jsx>{`
                 header {
-                    min-height: 95vh;
+                    height: 95vh;
                 }
-                .introHeaderBg {
-                    min-height: 95vh;
+                .introContent {
+                    height: 95vh;
                 }
                 // height = 100vh * tan(3) // rounded
                 .introEndBg {
@@ -19,31 +19,33 @@ export default function Section() {
                 }
             `}</style>
 
-            <div className="introHeaderBg text-center bg-primary fixed top-0 left-0 right-0">
-                <h1 className="text-base pt-16 pb-20">
-                    <span className="text-4xl font-extrabold">Salut ! Moi c'est Vivien, <br/>développeur web</span>
-                </h1>
-                <Container>
-                    <div className="w-3/4 md:w-1/2 mx-auto mb-12">
-                        <div className="mb-8 md:w-1/2 mx-auto">
-                            <Image src="/images/profilepic.png" width={255} height={287} alt="Là c'est moi" />
+            <div className="introContent text-center bg-primary fixed top-0 left-0 right-0">
+                <Container className="h-full">
+                    <div className="h-full flex flex-col justify-evenly py-10">
+                        <h1 className="text-base">
+                            <span className="text-4xl font-extrabold">Salut ! Moi c'est Vivien, <br/>développeur web</span>
+                        </h1>
+                        <div className="md:w-1/2 mx-auto">
+                            <Image src="/images/profilepic.png" width={255*.75} height={287*.75} alt="Là c'est moi" />
                         </div>
-                        <p className="mb-4">
-                            Développeur web depuis 12 ans, j'accompagne des entreprises et des agences dans la création de sites &amp; applications web.
-                        </p>
-                        <p className="mb-4">
-                            Mes outils de choix sont <strong>Laravel</strong> et <strong>Wordpress</strong>.
-                        </p>
-                        <p>
-                            Continuez pour en savoir plus ou allez voir mon CV.
-                        </p>
+                        <div className="w-3/4 mx-auto">
+                            <p className="mb-4">
+                                Développeur web depuis 12 ans, j'accompagne des entreprises et des agences dans la création de sites &amp; applications web.
+                            </p>
+                            <p className="mb-4">
+                                Mes outils de choix sont <strong>Laravel</strong> et <strong>Wordpress</strong>.
+                            </p>
+                            <p>
+                                Continuez pour en savoir plus ou allez voir mon CV.
+                            </p>
+                        </div>
+                        <nav>
+                            <NavLink href="#services">Services</NavLink>
+                            <NavLink href="#realisations">Réalisations</NavLink>
+                            <NavLink href="#contact">Contact</NavLink>
+                        </nav>
                     </div>
                 </Container>
-                <nav>
-                    <NavLink href="#services">Services</NavLink>
-                    <NavLink href="#realisations">Réalisations</NavLink>
-                    <NavLink href="#contact">Contact</NavLink>
-                </nav>
             </div>
 
             <div className="bg-white absolute left-0 right-0 z-10 items-center transform -skew-y-3 origin-top-left introEndBg"></div>
