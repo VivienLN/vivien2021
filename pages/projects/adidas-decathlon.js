85a6c19099a7d91336f1687261d39803ff4de037
VivienLN <vivienleneez@gmail.com>
2021-08-31 12:26:19 +0200

    projects wip

--- pages/projects/adidas-decathlon.js
+++ pages/projects/adidas-decathlon.js
@@ -4,6 +4,8 @@ import ProjectBackground from "../../components/projects/page/background"
 import ProjectHeader from "../../components/projects/page/header"
 import ProjectSummary from "../../components/projects/page/summary"
 import SummaryParagraph from "../../components/projects/page/summary-paragraph"
+import Video from "../../components/projects/page/video"
+import Image from "../../components/projects/page/image"
 import Container from "../../components/container"
 import Links from "../../components/projects/page/links"
 
@@ -17,6 +19,8 @@ export default function Project() {
                         <SummaryParagraph>En 2020, Adidas lance deux gammes de produits en matières recyclées.</SummaryParagraph>
                         <SummaryParagraph>Nous avons développé une page boutique dédiée sur Décathlon, ainsi qu'une page de présentation du procédé de recyclage et de fabrication de ces produits. Elles sont traduites en cinq langues.</SummaryParagraph>
                     </ProjectSummary>
+                    <Video url="/images/projects/adidas-decathlon/adidas-scroll.mp4" />
+                    <Image src="/images/projects/adidas-decathlon/ecodesign.jpg" width={960} height={815} />
                 </Container>
                 <Links slug="adidas-decathlon" />
             </ProjectBackground>
