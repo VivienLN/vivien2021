0cf182813d66e7b1942fa76d15c587309b0c6060
VivienLN <vivienleneez@gmail.com>
2021-08-31 15:19:46 +0200

    projects done

--- pages/projects/les-very-goods.js
+++ pages/projects/les-very-goods.js
@@ -4,6 +4,7 @@ import ProjectBackground from "../../components/projects/page/background"
 import ProjectHeader from "../../components/projects/page/header"
 import ProjectSummary from "../../components/projects/page/summary"
 import SummaryParagraph from "../../components/projects/page/summary-paragraph"
+import Image from "../../components/projects/page/image"
 import Container from "../../components/container"
 import Links from "../../components/projects/page/links"
 
@@ -18,6 +19,9 @@ export default function Project() {
                         <SummaryParagraph>On y trouve des guides d'achat sur deux sujets principaux : la tech et la maison.</SummaryParagraph>
                         <SummaryParagraph>Le back-office est construit sur-mesure pour les rédacteurs, et <strong>est connecté au catalogue amazon</strong> (via l'API partenaires), permettant la recherche et sélection de produit, la mise à jour des produits en rupture de stock, la sélection des variantes et leurs prix, etc.</SummaryParagraph>
                     </ProjectSummary>
+                    <Image src="/images/projects/les-very-goods/01.jpg" width={960} height={540} />
+                    <Image src="/images/projects/les-very-goods/02.jpg" width={960} height={540} />
+                    <Image src="/images/projects/les-very-goods/03.jpg" width={960} height={540} />
                 </Container>
                 <Links slug="les-very-goods" />
             </ProjectBackground>
--- pages/projects/ni-no-kuni-2.js
+++ pages/projects/ni-no-kuni-2.js
@@ -4,6 +4,8 @@ import ProjectBackground from "../../components/projects/page/background"
 import ProjectHeader from "../../components/projects/page/header"
 import ProjectSummary from "../../components/projects/page/summary"
 import SummaryParagraph from "../../components/projects/page/summary-paragraph"
+import Video from "../../components/projects/page/video"
+import Image from "../../components/projects/page/image"
 import Container from "../../components/container"
 import Links from "../../components/projects/page/links"
 
@@ -17,6 +19,9 @@ export default function Project() {
                         <SummaryParagraph>Pour la sortie du jeu Ni No Kuni 2, nous avons développé le site officiel en une page.</SummaryParagraph>
                         <SummaryParagraph>Le site devait être 100% statique et pouvoir tourner sans aucun code serveur, tout en offrant une expérience immersive en six langues.</SummaryParagraph>
                     </ProjectSummary>
+                    <Video url="/images/projects/ni-no-kuni-2/home.mp4" />
+                    <Video url="/images/projects/ni-no-kuni-2/scroll.mp4" />
+                    <Image src="/images/projects/ni-no-kuni-2/menu.jpg" width={960} height={540} />
                 </Container>
                 <Links slug="ni-no-kuni-2" />
             </ProjectBackground>
