4497215463df91422c04952736d3ba210fcd6b90
VivienLN <vivienleneez@gmail.com>
2021-09-15 17:47:30 +0200

    add transition on load

--- components/projects/page/hero-banner.js
+++ components/projects/page/hero-banner.js
@@ -2,6 +2,7 @@ import Container from "../../container"
 import ScrollFactor from "../../scroll-factor"
 
 export default function HeroBanner(props) {
+    console.log("isVisible", props.isVisible)
     return (
             <div className="heroBanner relative">
                 <style jsx>{`
@@ -13,6 +14,27 @@ export default function HeroBanner(props) {
                         background-size: cover;
                         height: 100vh;
                     }
+                    .content > * {
+                        display: block;
+                        opacity: 0; 
+                        animation: 1.2s cubic-bezier(0.165, 0.840, 0.440, 1.000) .4s 1 forwards showTitle;
+                    }
+                    .content > *:nth-child(2) {
+                        animation-delay: .8s
+                    }
+
+                    @keyframes showTitle {
+                        from { 
+                            transform: translateY(100%);
+                            opacity: 0; 
+                            clip-path: polygon(0 0, 100% 0, 100% 0, 0 0);
+                        }
+                        to { 
+                            transform: translateY(0); 
+                            opacity: 1; 
+                            clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%); 
+                        }
+                    }
                 `}</style>
                 <div className="absolute left-0 right-0 bottom-0 top-0 overflow-hidden">
                     <ScrollFactor ratio={.1}>
@@ -22,8 +44,10 @@ export default function HeroBanner(props) {
                 <Container className="h-full">
                     <div className="py-20 h-full flex flex-col justify-center filter drop-shadow-xl">
                         <ScrollFactor ratio={0.4}>
-                            <h1 className="text-2xl lg:text-4xl font-extrabold mb-8">{ props.title }</h1>
-                            <p className="lg:text-xl font-extrabold text-primary">{ props.subtitle }</p>
+                            <div className="content">
+                                <h1 className="text-2xl lg:text-4xl font-extrabold mb-8">{ props.title }</h1>
+                                <p className="lg:text-xl font-extrabold text-primary">{ props.subtitle }</p>
+                            </div>
                         </ScrollFactor>
                     </div>
                 </Container>
