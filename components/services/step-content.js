cfbdee2a4487eec688cd0080771b1f296d6254a7
VivienLN <vivienleneez@gmail.com>
2021-09-15 14:44:34 +0200

    fix indentation

--- components/services/step-content.js
+++ components/services/step-content.js
@@ -2,13 +2,13 @@ import InViewTransition from '../in-view-transition'
 
 export default function Step(props) {
     return (
-            <div className="text-sm md:text-base px-6">
-                <InViewTransition delay={100}>
-                    <h3 className="text-xl mb-4 font-extrabold">{props.title}</h3>
-                </InViewTransition>
-                <InViewTransition delay={600}>
-                    {props.children}
-                </InViewTransition>
-            </div>
+        <div className="text-sm md:text-base px-6">
+            <InViewTransition delay={100}>
+                <h3 className="text-xl mb-4 font-extrabold">{props.title}</h3>
+            </InViewTransition>
+            <InViewTransition delay={600}>
+                {props.children}
+            </InViewTransition>
+        </div>
     )
 }
