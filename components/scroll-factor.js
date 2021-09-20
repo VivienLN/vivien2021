cd9d9389445602b20ad23ae320abed53439929fd
VivienLN <vivienleneez@gmail.com>
2021-09-20 12:28:10 +0200

    replace scroll event with RAF in scrollfactor component

--- components/scroll-factor.js
+++ components/scroll-factor.js
@@ -1,27 +1,33 @@
 import { createRef, useRef, useState, useEffect } from 'react'
 
 export default function ScrollFactor(props) {
-    const wrapperRef = useRef(null)
+    const wrapperRef = useRef()
+    const requestRef = useRef()
     const [offset, setOffset] = useState(0)
+
+    var originalY = null
+    const getCenterY = () =>  {
+        let bounds = wrapperRef.current.getBoundingClientRect()
+        return bounds.top + (bounds.bottom - bounds.top) / 2
+    }
+
+    const updateOffset = () => {
+        if(!wrapperRef.current) {
+            return
+        }
+        originalY = (originalY === null) ? getCenterY() : originalY
+        let windowHeight = window.innerHeight
+        let posY = getCenterY()
+        let origin = (originalY < windowHeight && originalY > 0) ? originalY : windowHeight / 2
+        setOffset(-(props.ratio - 1) * (origin  - posY))
+
+        requestRef.current = requestAnimationFrame(updateOffset)
+    }
 
     useEffect(() => {
         if(typeof window !== "undefined") {
-            const getCenterY = () =>  {
-                let bounds = wrapperRef.current.getBoundingClientRect()
-                return bounds.top + (bounds.bottom - bounds.top) / 2
-            }
-            const originalY = getCenterY()
-
-            const handleScroll = event => {
-                let windowHeight = window.innerHeight
-                let posY = getCenterY()
-                // The position where the element offset is 0
-                let origin = (originalY < windowHeight && originalY > 0) ? originalY : windowHeight / 2;
-                setOffset(-(props.ratio - 1) * (origin  - posY))
-            }            
-            handleScroll()
-            window.addEventListener('scroll', handleScroll)
-            return () => window.removeEventListener("scroll", handleScroll)
+            requestRef.current = requestAnimationFrame(updateOffset)
+            return () => cancelAnimationFrame(requestRef.current)
         }
     }, [])
 
